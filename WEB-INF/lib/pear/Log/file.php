69722288b49c236d3f747648cefa6b498e2daf7c
gflamerich <gflamerich@2f0d8748-a8d7-424d-8b13-b8ac898be960>
2010-02-17 18:11:23 +0000

    * Actualización del Pear Log versión 1.12.0

--- WEB-INF/lib/pear/Log/file.php
+++ WEB-INF/lib/pear/Log/file.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * $Header: /repository/pear/Log/Log/file.php,v 1.45 2006/01/11 07:56:37 jon Exp $
+ * $Header$
  *
- * @version $Revision: 1.45 $
+ * @version $Revision: 224513 $
  * @package Log
  */
 
@@ -250,7 +250,11 @@ class Log_file extends Log
      */
     function flush()
     {
-        return fflush($this->_fp);
+        if (is_resource($this->_fp)) {
+            return fflush($this->_fp);
+        }
+
+        return false;
     }
 
     /**
