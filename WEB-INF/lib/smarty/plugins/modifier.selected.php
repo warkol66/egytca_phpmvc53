c4bb0ed5f49fc16555cfa57f1d5a0dad13e31dca
gflamerich <gflamerich@2f0d8748-a8d7-424d-8b13-b8ac898be960>
2010-09-06 14:27:30 +0000

    * Fix del modifier selected para que compare pro tipo solo los valores null o cero

--- WEB-INF/lib/smarty/plugins/modifier.selected.php
+++ WEB-INF/lib/smarty/plugins/modifier.selected.php
@@ -8,7 +8,7 @@
  * -------------------------------------------------------------
  */
 function smarty_modifier_selected($value1,$value2){
-	if ($value1 === $value2)
+	if (($value1 == $value2) && (($value1 !== 0 && $value2 !== NULL) && ($value2 !== 0 && $value1 !== NULL)))
 		return 'selected="selected"';
-	return '';
+	return;
 }
