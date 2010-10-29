e62f6284fc701da621ecff59b05094f541f28c28
gflamerich <gflamerich@2f0d8748-a8d7-424d-8b13-b8ac898be960>
2010-10-29 00:57:29 +0000

    * Corrijo plug ing para que funcione como en el base actio, verificando permisos según el modulo si no existe el action

--- WEB-INF/lib/smarty/plugins/modifier.security_user_has_access.php
+++ WEB-INF/lib/smarty/plugins/modifier.security_user_has_access.php
@@ -10,14 +10,19 @@
  */
 function smarty_modifier_security_user_has_access($action)
 {
-	$loginUser = $_SESSION["loginUser"];
 
-	$loginUserAffiliate = $_SESSION["loginAffiliateUser"];
-
-	$loginUserRegistration = $_SESSION["loginRegistrationUser"];
+	if (isset($_SESSION["loginUser"]))
+		$loginUser = $_SESSION["loginUser"];
+	else if (isset($_SESSION["loginAffiliateUser"]))
+		$loginUserAffiliate = $_SESSION["loginAffiliateUser"];
+	else if (isset($_SESSION["loginRegistrationUser"]))
+		$loginRegistrationUser = $_SESSION["loginRegistrationUser"];
 
 	if (!empty($loginUserAffiliate))
 		$user = $loginUserAffiliate;
+
+	else if (!empty($loginUserRegistration))
+		$user = $loginUserRegistration;				
 
 	if (!empty($loginUser)) {
 		$user = $loginUser;	
@@ -26,18 +31,31 @@ function smarty_modifier_security_user_has_access($action)
 			return true;
 	}
 
-	if (!empty($loginUserRegistration))
-		$user = $loginUserRegistration;				
-	
-	$actualAction = SecurityActionPeer::getByNameOrPair($action);	
+	if (preg_match('/^([a-z]*)[A-Z]/',$action,$regs))
+		$moduleRequested = $regs[1];
 
-	//si el action no existe, el usuario no tiene acceso
-	if (empty($actualAction))
-		return false;
+	$securityAction = SecurityActionPeer::getByNameOrPair($action);
+	$securityModule = SecurityModulePeer::get($moduleRequested);
+
+	//Controlo las acciones y modulos que no requieren login
+	//Si no se requiere login $noCheckLogin va a ser igual a 1
+	$noCheckLogin = 1;
+	if (!empty($securityAction))
+		$noCheckLogin = $securityAction->getOverallNoCheckLogin();
+	else if (!empty($securityModule))
+		$noCheckLogin = $securityModule->getNoCheckLogin();
+	else
+		$noCheckLogin = 0;
+
+	if (ConfigModule::get("global","noCheckLogin") || $noCheckLogin)
+		return true;
+
+	if (!empty($loginUser) || !empty($loginUserAffiliate) || !empty($loginRegistrationUser)) {
+		if (!empty($securityAction))
+			$access = $securityAction->getAccessByUser($user);
+		else if (!empty($securityModule))
+			$access = $securityModule->getAccessByUser($user);
+	}
 	
-	$hasAccess = $actualAction->hasAccess($user);
-	
-	return $hasAccess;
+	return $access;
 }
-
-?>
